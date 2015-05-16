fe1c0ae580266d9f3e9b5e823b0489588d723891
tophj <tophj@vt.edu>
2015-05-16 12:11:10 -0400

    reorg

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,38 +1,37 @@
-/* 
----------------------- contentscript.js -------------------------
- ------  Shows all the non-spam comments on the webpage ---------
- */ 
-
-/* Work-around for running functions defined outside the DOM */
+/**
+ * Shows all comments in a thread. Defined on the server side.
+ */
 function main(){
   showAllComments();
 }
+
+
+/**
+ * Work-around for running the showAllComments() function, which is defined server-side
+*/
 var script = document.createElement('script');
 script.appendChild(document.createTextNode('('+ main +')();'));
 document.head.appendChild(script);
 
 
-/* Loops through HTML and extracts all the comment links */
+/**
+ * Parses HTML to find the summary div, and all relevant comment divs
+ */
 $(function(){
   // Censors the summary
   var summary = $("li").find("div");
   for(var i = 0; i < summary.length; i++){
-
     var parentClass = $(summary[i]).parent().attr('class');
 
     if(parentClass === 'cfix mb_1'){
       summary[i].style.background = 'black';
     }
   }
-
   // Censors the comments
   var comments = $("li").find("p");
   for(var i = 0; i < comments.length ; i++){
     var parentName = $(comments[i]).parent().attr('name');
-
     if(parentName != "fcomments"){
-
-      // can look here for i links which represent spam too
       var textNode = checkSpam(comments[i]);
       censorText(textNode);
     }
@@ -40,26 +39,27 @@ $(function(){
 });
 
 
-/* Expands a comment and checks if it's spam or not */
+/**
+ * Most comments with links are automatically tagged as spam
+ * This unhides the comment and checks to see if it contains a link, or is actually spam.
+ */
 function checkSpam(textNode){
 
   var v = textNode.innerHTML;
-  var checkSpam = "This comment is hidden because it's likely to be inappropriate or spam.";
+  var checkSpam = "This comment is hidden because it's likely to be inappropriate or spam. ";
   var spam = v.search(checkSpam);
-  
-
   if(spam >= 0){   
     var button = $(textNode).find("a");
     if(button[0] != null){
-       button[0].click();
-
-    } 
-    
+       $(button[0]).click();
+    }
   }
   return textNode;
 }
 
-/* Replaces all non-links with a black bar */
+/**
+ * Changes the CSS of all non-links(spoilers) to have a black background and thus be unreadable
+ */
 function censorText(textNode) {
 
   var v = textNode.innerHTML;
@@ -79,14 +79,11 @@ function censorText(textNode) {
       targetLength = aLinks.length;
       findLinks = aLinks;
     }
-
     for(var i = 0; i < targetLength;i++){
       returnString += "<a href=" + findLinks[i] + ">" + findLinks[i] + "</a><br>" ; 
     }
-    
     v = returnString;
   }  
-  
   textNode.innerHTML = v;
 
   if(changed){
